3ed4be95b01f43e19796a78a45d4825123c24287
Julien Cresus <julien.cresus@methods.co.uk>
2022-03-02 14:51:50 +0000

    Added a Procfile.

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,11 @@
 let gulp = require('gulp');
-gulp.task('heroku:production', function() {
-  console.log('hello'); // the task does not need to do anything.
+gulp.task('serve', function() {
+  browserSync({
+    server: {
+      baseDir: './',
+    },
+    port: process.env.PORT || 5000,
+  });
+
+  gulp.watch(['*.html', 'css/*.css', 'js/*.js', 'views/*.html', 'template/*.html', './*.html'], { cwd: 'app' }, reload);
 });
